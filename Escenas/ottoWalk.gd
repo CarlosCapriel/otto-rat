e121cde846a5b938deeb4e0cd8d47847fa55c930
FernandoDCastillo <al054435@uacam.mx>
2022-03-28 10:30:44 -0600

    Se creo el movimiento de otto y una parte del escenario

---
+++ Escenas/ottoWalk.gd
@@ -0,0 +1,38 @@
+extends KinematicBody2D
+ 
+const ACCELERATION = 70
+const MAX_SPEED = 300
+const JUMP_H = -900
+const UP = Vector2(0, -1)
+const gravity = 40
+ 
+onready var animationSprite = $AnimatedSprite
+ 
+var motion = Vector2()
+ 
+func _physics_process(delta):
+	# apply gravity to the player
+	motion.y += gravity
+	var friction = false
+ 
+	if Input.is_action_pressed("ui_right"):
+		animationSprite.play("caminar")
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+	elif Input.is_action_pressed("ui_left"):
+		animationSprite.play("caminar")
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+	else:
+		animationSprite.stop()
+		friction = true
+ 
+	if is_on_floor():
+ 
+		if Input.is_action_just_pressed("ui_accept"):
+			motion.y = JUMP_H
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.5)
+	else:
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.01)
+ 
+	motion = move_and_slide(motion, UP)
