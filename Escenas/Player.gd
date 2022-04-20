b7baa6ddd7113686faae97da56c2371d755a327d
Sarahi1705 <89057328+Sarahi1705@users.noreply.github.com>
2022-04-19 19:01:32 -0500

    se hizo los numeros (#22)

--- Escenas/Player.gd
+++ Escenas/Player.gd
@@ -8,8 +8,9 @@ var Movimiento = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#limite = get_viewport_rect().size
 	pass
+	#limite = get_viewport_rect().size
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
